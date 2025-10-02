c7510cadad3325a7aaebfe6f2d27e6fdbf9792ee
JJRubi <rubi.begue09@gmail.com>
2025-10-01 21:37:38 -0600

    Configuracion de firebaseoptions de app pwa

---
+++ apps/valet_pwa/lib/firebase_options.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions{
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: "AIzaSyCTSWg1isO7d_zgSKeMTwIjB8NSp_gBU0o",
+    authDomain: "valetflowqr-40544.firebaseapp.com",
+    projectId: "valetflowqr-40544",
+    storageBucket: "valetflowqr-40544.firebasestorage.app",
+    messagingSenderId: "835667889247",
+    appId: "1:835667889247:web:6d86da70f314e33dea9fea"
+  );
+}
